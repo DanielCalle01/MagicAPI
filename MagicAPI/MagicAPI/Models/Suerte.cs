fcb36f9507b7ccebcab078644663433a4e1dd037
DanielCalle01 <a2019211611@estudiantes.upsa.edu.bo>
2021-10-13 21:14:18 -0400

    Modelo Suerte creado

---
+++ MagicAPI/MagicAPI/Models/Suerte.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicAPI.Models
+{
+    public class Suerte
+    {
+        [Key]
+        public string FutureId { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Vision { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")]
+        [Url]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Imagen { get; set; }
+    }
+}
