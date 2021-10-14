f356dee6dc1c403e0b418e0fcdd6fa174614321b
DanielCalle01 <a2019211611@estudiantes.upsa.edu.bo>
2021-10-13 21:27:25 -0400

    Migrations implementado

---
+++ MagicAPI/MagicAPI/Migrations/20211014012557_AddModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MagicAPI.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suerte",
+                columns: table => new
+                {
+                    FutureId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Vision = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suerte", x => x.FutureId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suerte");
+        }
+    }
+}
